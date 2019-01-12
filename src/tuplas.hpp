1ba7a4e5490925e757335b57c9e1204c779f8f4b
ZgzInfinity <38316325+ZgzInfinity@users.noreply.github.com>
2019-01-12 23:00:38 +0100

    comentado modulo tuplas

--- src/tuplas.hpp
+++ src/tuplas.hpp
@@ -1,3 +1,19 @@
+/*
+ * ----------------------------------------------------------
+ * -- Programación de sistemas concurrentes y distribuidos --
+ * -- Trabajo práctico : Servidor Linda ---------------------
+ * -- Autores y NIP -----------------------------------------
+ * -- Daniel Naval Alcalá  739274 ---------------------------
+ * -- Alejandro Omist Casado 737791 -------------------------
+ * -- Rubén Rodríguez Esteban 737215 ------------------------
+ * -- José Manuel Romero Clavería 74 ------------------------
+ * ----------------------------------------------------------
+ */
+
+/*
+ * Fichero de interfaz del módulo Tupla.
+ */
+
 #ifndef _TUPLAS_HPP_
 #define _TUPLAS_HPP_
 
@@ -9,10 +25,12 @@ using namespace std;
 class Tupla
 {
    private:
-    int dimension;
-    string vector[6];
+    int dimension;         // Dimensión de la tupla
+    string vector[6];      // Estructura estática para almacenar las componentes de la tupla
 
    public:
+    // Colección de funciones de la clase especificadas en 
+    // el fichero de implementación del módulo tuplas
     Tupla(string p1);
     Tupla(string p1, string p2);
     Tupla(string p1, string p2, string p3);
