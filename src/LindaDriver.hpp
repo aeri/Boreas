c8ed716855867b3aa3bd3b346a225b53ea374ef5
josman231 <josman.romero@gmail.com>
2019-01-12 21:17:56 +0100

    Update LindaDriver.hpp

--- src/LindaDriver.hpp
+++ src/LindaDriver.hpp
@@ -9,16 +9,16 @@
 class LD : public Socket
 {
    private:
-    int port;
-    string ip;
-    int socket_fd;
+    int port; //puerto de conexión 
+    string ip; //dirección ip
+    int socket_fd; //socket de conexión
 
    public:
-    LD(string ip, string p);
-    void PN(Tupla t);
-    Tupla RN(Tupla t);
-    Tupla ReadN(Tupla t);
-    void STOP();
+    LD(string ip, string p); //constructor 
+    void PN(Tupla t); //PostNote
+    Tupla RN(Tupla t); //RemoveNote
+    Tupla ReadN(Tupla t); //ReadNote
+    void STOP();//cierra la conexión del socket
 };
 
 #endif
