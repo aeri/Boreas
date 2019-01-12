c23fe3cc8e8e167edbd34fadc3534a1e8135dcfb
ZgzInfinity <38316325+ZgzInfinity@users.noreply.github.com>
2019-01-12 22:57:14 +0100

    comentado tuplas

--- src/tuplas.cpp
+++ src/tuplas.cpp
@@ -1,3 +1,19 @@
+/*
+ * ----------------------------------------------------------
+ * -- Programación de sistemas concurrentes y distribuidos --
+ * -- Trabajo práctico : Servidor Linda ---------------------
+ * -- Autores y NIP -----------------------------------------
+ * -- Daniel Naval Alcalá  739274 ---------------------------
+ * -- Alejandro Omist Casado 737791 -------------------------
+ * -- Rubén Rodríguez Esteban 737215 ------------------------
+ * -- José Manuel Romero Clavería 74 ------------------------
+ * ----------------------------------------------------------
+ */
+
+/*
+ * Fichero de implementación del módulo Tupla.
+ */
+
 #include <sstream>
 #include "tuplas.hpp"
 
