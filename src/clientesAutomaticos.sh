c667e6229e015a9975c615d0fff6d77de11e9e2d
ZgzInfinity <38316325+ZgzInfinity@users.noreply.github.com>
2019-01-13 19:00:04 +0100

    script comentado

--- src/clientesAutomaticos.sh
+++ src/clientesAutomaticos.sh
@@ -1,8 +1,19 @@
+#
+# ----------------------------------------------------------
+# -- Programación de sistemas concurrentes y distribuidos --
+# -- Trabajo práctico de Servidor Linda -------------------- 
+# -- Autores -----------------------------------------------
+# -- Daniel Naval Alcalá 739274 ----------------------------
+# -- Alejandro Omist Casado 739971 -------------------------
+# -- Rubén Rodríguez Esteban 737215 ------------------------
+# -- José Manuel Romero Clavería 740914 --------------------
+#
+
 #!/bin/bash
 
+# Lanzamiento de diez clientes interactivos
 for i in $(seq 1 10)
 do
 	gnome-terminal -e "./clienteInteractivo localhost 30000"
 done
 
-./cargacl localhost 30000
